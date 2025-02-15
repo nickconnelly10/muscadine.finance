b406a7fd93b1ba2e09f3b006e869e221fd5a19dd
nickconnelly10 <nickconnelly10@gmail.com>
2025-02-15 18:22:24 -0500

    test6

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
   },
 };
